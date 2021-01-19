dd902cd800c46f7849fff423b16212c01c0eac37
AntonVoitsekhovskii <62042802+AntonVoitsekhovskii@users.noreply.github.com>
2021-01-19 18:45:28 -0500

    deleted workbarnch and created devl branch

--- src/Class2.java
+++ src/Class2.java
@@ -4,5 +4,6 @@ public class Class2 {
 	public static void main(String[] args) {
 		System.out.println("Eclipse IDE");
 		System.out.println("New Line in Class 2");
+		System.out.println("new branch");
 	}
 }
