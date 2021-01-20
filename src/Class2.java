7e82ce7782d516ebe6b23ff71f54b93bd0dde572
AntonVoitsekhovskii <62042802+AntonVoitsekhovskii@users.noreply.github.com>
2021-01-19 19:19:58 -0500

    added 2 lines in Class2

--- src/Class2.java
+++ src/Class2.java
@@ -4,5 +4,7 @@ public class Class2 {
 	public static void main(String[] args) {
 		System.out.println("Eclipse IDE");
 		System.out.println("New Line in Class 2");
+		System.out.println("create git dash example");
+		System.out.println("++++++++++++++++++++");
 	}
 }
